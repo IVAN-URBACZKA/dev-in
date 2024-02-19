bf37e0fde8b57c8bef1d47ec3a45def4cfa3430f
IVAN-URBACZKA <ivan.urbaczka@gmail.com>
2024-02-19 08:21:22 +0100

    add link for article card

--- front-end/components/ArticleCard.tsx
+++ front-end/components/ArticleCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { TERipple } from 'tw-elements-react';
+import Link from 'next/link'
+
 
 interface CardBasicProps {
     id: number,
@@ -11,7 +13,7 @@ interface CardBasicProps {
 export default function CardBasicExample({id, name, content, createdAt}: CardBasicProps): JSX.Element {
   return (
       <div
-        className="mx-auto block max-w-md p-6 rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl">
+        className="mx-auto mb-5 block max-w-md p-6 rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl">
         <h5
           className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
           {name}
@@ -19,13 +21,8 @@ export default function CardBasicExample({id, name, content, createdAt}: CardBas
         <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
           {content}
         </p>
-        <TERipple>
-          <button
-            type="button"
-            className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
-            En savoir plus
-          </button>
-        </TERipple>
+        <em>{createdAt}</em> <br />
+        <Link href={`/articles/${id}`}>Cliquez ici</Link>
       </div>
   );
 }
