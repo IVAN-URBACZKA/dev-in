2917ed1a24f3cd47624dcd9c2d51842335324e24
IVAN-URBACZKA <ivan.urbaczka@gmail.com>
2024-02-20 13:50:32 +0100

    add header in archive page

--- front-end/app/archives/page.tsx
+++ front-end/app/archives/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 import React, { Component } from 'react'
+import Header from '@/components/Header'
 
 export default class page extends Component {
   render() {
     return (
+        <>
+    <Header />
       <div>
         <h1>ARCHIVES</h1>
       </div>
+      </>
     )
   }
 }
