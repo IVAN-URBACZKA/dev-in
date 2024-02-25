982af1a692cd3bc059174e2d8ad15c2e2b0f786b
IVAN-URBACZKA <ivan.urbaczka@gmail.com>
2024-02-25 07:43:23 +0100

    truncated text

--- front-end/components/ArticleCard.tsx
+++ front-end/components/ArticleCard.tsx
@@ -9,10 +9,17 @@ interface CardBasicProps {
     createdAt: string
 }
 
+function TruncateText(text: string, maxLength: number): string {
+  return text.length > maxLength ? `${text.substring(0, maxLength)}...` : text;
+}
+
 export default function CardBasicExample({id, name,slug, content, createdAt}: CardBasicProps): JSX.Element {
   const formattedDate = new Date(createdAt).toLocaleDateString("fr-FR", {
     day: "numeric", month: "long", year: "numeric"
   });
+
+  const maxLength = 100; 
+  const truncatedText = TruncateText(content, maxLength);
 
   return (
       <div className="mx-auto mt-5 mb-8 block max-w-md p-8 rounded-xl shadow-sm white:bg-gray-800 grey:border-gray-700 border border-gray-200 md:max-w-3xl">
@@ -21,7 +28,7 @@ export default function CardBasicExample({id, name,slug, content, createdAt}: Ca
             <Link href={`/articles/${slug}`}>{name}</Link>
             </h5>
             <p className="text-lg text-gray-700 dark:text-gray-300">
-              {content}
+              {truncatedText}
             </p>
             <em className="text-sm text-gray-500">{formattedDate}</em> <br />
           </div>
