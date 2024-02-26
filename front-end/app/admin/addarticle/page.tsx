4a5f998a5e77415c77ee8c946c1af1ec898678b6
IVAN-URBACZKA <ivan.urbaczka@gmail.com>
2024-02-26 06:56:55 +0100

    add yup and formik on my forms

--- front-end/app/admin/addarticle/page.tsx
+++ front-end/app/admin/addarticle/page.tsx
@@ -1,79 +1,77 @@
 "use client";
-import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
-import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import ApiService from '@/services/ApiService';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
-interface Article {
-  name: string,
-  content: string,
-}
+
+const validationSchema = Yup.object({
+  name: Yup.string().required('Le nom est obligatoire'),
+  content: Yup.string().required('Le contenu est obligatoire'),
+});
 
 const CreateArticleForm: React.FC = () => {
-  const [article, setArticle] = useState<Article>({ name: '', content: '' });
   const router = useRouter();
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-
     if (!token) {
-      router.push('/login')
+      router.push('/login');
     }
-  }, [router]); // 
+  }, [router]);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setArticle({ ...article, [name]: value });
-  };
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    try {
-      const response = await ApiService.addArticle(article);
-      router.push('/')
-
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      content: '',
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      try {
+        await ApiService.addArticle(values);
+        router.push('/');
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
 
   return (
-    <>
-  <div className="flex justify-center items-center h-screen">
-  <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-lg"> {/* Ajustez la largeur du formulaire si nécessaire */}
-    <div className="mb-4">
-      <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Nom</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        value={article.name}
-        onChange={handleChange}
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      />
+    <div className="flex justify-center items-center h-screen">
+      <form onSubmit={formik.handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-lg">
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Nom</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+          {formik.errors.name && formik.touched.name && <div className="text-red-500 text-xs italic">{formik.errors.name}</div>}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="content" className="block text-gray-700 text-sm font-bold mb-2">Contenu</label>
+          <textarea
+            id="content"
+            name="content"
+            onChange={formik.handleChange}
+            value={formik.values.content}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            style={{ minHeight: '300px' }}
+            rows={10}
+          ></textarea>
+          {formik.errors.content && formik.touched.content && <div className="text-red-500 text-xs italic">{formik.errors.content}</div>}
+        </div>
+        <div className="flex items-center justify-between">
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Créer l'article
+          </button>
+        </div>
+      </form>
     </div>
-    <div className="mb-4">
-      <label htmlFor="content" className="block text-gray-700 text-sm font-bold mb-2">Contenu</label>
-      <textarea
-        id="content"
-        name="content"
-        value={article.content}
-        onChange={handleChange}
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        style={{ minHeight: '300px' }} // Définissez une hauteur minimale ici
-        rows={10}
-      ></textarea>
-    </div>
-
-    <div className="flex items-center justify-between">
-      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-        Créer l'article
-      </button>
-    </div>
-  </form>
-</div>
-
-    </>
   );
 };
 
--- front-end/app/login/page.tsx
+++ front-end/app/login/page.tsx
@@ -1,88 +1,79 @@
 "use client"
-import React, { useState, ChangeEvent, FormEvent, use, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
-import { useRouter } from 'next/navigation'
-
-interface IUser {
-    email: string;
-    password: string;
-  }
+import { useRouter } from 'next/navigation';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const LoginForm: React.FC = () => {
-
-    const [user, setUser] = useState<IUser>({ email: '', password: '' });
-    const [login, setLogin] = useState<boolean>(false);
     const router = useRouter(); 
-    const token = localStorage.getItem('token');
 
     useEffect(() => {
       const token = localStorage.getItem('token');
       if (token) {
         router.push('/')
       }
-    }, [router] )
-    
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-    
-        try {
-          const response = await axios.post('http://localhost:8000/api/login', user);
-          if(response.status === 200){
-            setLogin(true);
-            localStorage.setItem('token', response.data.token );
-            router.push('/form/addarticle')
-          }
-          
-        } catch (error) {
-          console.error(error);
-          
-        }
-      };
+    }, [router])
 
-      const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setUser({ ...user, [name]: value });
-      };
-
-     
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().email('Email invalide').required('L\'email est obligatoire'),
+            password: Yup.string().required('Le mot de passe est obligatoire'),
+        }),
+        onSubmit: async (values) => {
+            try {
+                const response = await axios.post('http://localhost:8000/api/login', values);
+                if(response.status === 200){
+                    localStorage.setItem('token', response.data.token);
+                    router.push('/admin/addarticle');
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        },
+    });
 
     return (
         <>
-         { !token  ? (<div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-     
+            <div className="flex justify-center items-center h-screen">
+                <form onSubmit={formik.handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                        {formik.touched.email && formik.errors.email && <p className="text-red-500 text-xs italic">{formik.errors.email}</p>}
+                    </div>
 
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={user.email}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
+                    <div className="mb-6">
+                        <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        />
+                        {formik.touched.password && formik.errors.password && <p className="text-red-500 text-xs italic">{formik.errors.password}</p>}
+                    </div>
 
-        <div className="mb-6">
-          <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={user.password}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-
-        <div className="flex items-center justify-between">
-          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-            Se connecter
-          </button>
-        </div>
-      </form>
-    </div> ) : ( <p className="text-lg text-gray-500">VOUS ÊTEZ DEJA CONNECTER</p> )}
+                    <div className="flex items-center justify-between">
+                        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            Se connecter
+                        </button>
+                    </div>
+                </form>
+            </div>
         </>
     );
 };
